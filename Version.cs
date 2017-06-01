b2ac2c0be4d4f917a8b3f32c0d38f53e93a04351
Francesco Gabbanini <gabbanini_francesco@lilly.com>
2017-06-01 17:45:02 +0200

    Bump version to 2.2.3

--- Version.cs
+++ Version.cs
@@ -12,7 +12,7 @@ namespace PIDataReaderLib {
 	 * */
 
 	public class Version {
-		public static readonly string version = "2.2.2";
+		public static readonly string version = "2.2.3";
 		public static string getVersion() {
 			return String.Format("Using PIDataReaderLib v{0}.", version);
 		}
