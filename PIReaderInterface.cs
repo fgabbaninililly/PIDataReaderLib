9af4c511b1b5c73bfe1a9610baae963a7c84f521
fgabbaninililly <gabbanini_francesco@lilly.com>
2017-10-09 17:33:42 +0200

    Add method for reading batch hierarchies in PIReaderInterface. Method throws an exception in PIAFReader (cannot read batches using AF SDK).

--- PIReaderInterface.cs
+++ PIReaderInterface.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace PIDataReaderLib {
 	public interface PIReaderInterface {
 		PIData Read(string tagListCsvString, string phaseTagListCsvString, DateTime startTime, DateTime endTime);
+		PIData ReadBatchTree(DateTime startTime, DateTime endTime, string modulePath, bool readBatches, bool readUnitBatches, bool readSubBatches, bool readPhases);
 		long GetLastReadRecordCount();
 		//DateTime getReadFinishedTime();
 	}
