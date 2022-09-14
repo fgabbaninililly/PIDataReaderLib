a15fe6dc1685653368d1f3dcac0205d2bf497d17
fgabbaninililly <gabbanini_francesco@lilly.com>
2022-09-14 15:46:31 +0200

    Add buffered writing capability when serializing

--- TagSerializer.cs
+++ TagSerializer.cs
@@ -62,6 +62,7 @@ namespace PIDataReaderLib {
 				
 				foreach (Tag tag in tags) {
 					try {
+						//serializeTagToLocalFileBuffered(tag, outFile);
 						serializeTagToLocalFile(tag, outFile);
 					} catch (Exception e) {
 						logger.Error("Error serializing tag to file. Details: {0}", e.ToString());
@@ -81,6 +82,87 @@ namespace PIDataReaderLib {
 			sw.Flush();
 		}
 
+		private void serializeTagToLocalFileBuffered(Tag tag, StreamWriter outFile) {
+			//sample tag values: 2017-02-13T11-47-16:236.375,2017-02-13T11-48-12:No Data
+			//sample csv line should be: ITP_MTC_AE-066-491-01.P10.PV, 2016-08-22 11:49:03, -9.2926378, null, 0
+
+			if (null == tag.tagvalues || 0 == tag.tagvalues.Length) {
+				return;
+			}
+			List<string> tagListItems = new List<string>(tag.tagvalues.Split(new string[] { valueSeparator }, StringSplitOptions.None));
+			StringBuilder lineBuilder = new StringBuilder();
+			try {
+				ulong lineCnt = 0;
+				foreach (string tagInfo in tagListItems) {
+					string[] tagInfoArray = tagInfo.Split(new string[] { timeValueSeparator }, StringSplitOptions.None);
+
+					if (tagInfoArray.Length < 2) {
+						logger.Error("Invalid measurement format detected for Tag {0}. Measurement string is {1}.", tag.name, tagInfo);
+						continue;
+					}
+
+					string dateStr = tagInfoArray[0];
+					string valueStr = null;
+					string svalueStr = null;
+					string statusString = null;
+
+					//if (tagInfoArray[1].Contains('|')) {
+					if (tagInfoArray[1].Contains(fieldSeparator)) {
+						string[] valueStrArray = tagInfoArray[1].Split(new string[] { fieldSeparator }, StringSplitOptions.None);
+						valueStr = valueStrArray[0];
+						svalueStr = valueStrArray[1];
+						statusString = valueStrArray[2];
+					} else {
+						valueStr = tagInfoArray[1];
+						statusString = "0";
+						if (tag.getIsPhaseTag()) {
+							statusString = valueStr;
+							valueStr = null;
+						}
+
+						if (TypeUtil.Instance.isString(tag.valueType)) {
+							svalueStr = valueStr;
+							valueStr = null;
+						}
+					}
+
+					try {
+
+						DateTime dt = DateTime.ParseExact(dateStr, inDateFormat, CultureInfo.InvariantCulture);
+						string hadoopDate = dt.ToString(outDateFormat);
+
+						lineBuilder.AppendFormat("{0}{1}{2}{1}{3}{1}{4}{1}{5}{1}{6}\r\n",
+							 tag.name,              //0 tag
+							 csvSeparator,          //1
+							 hadoopDate,            //2 time
+							 valueStr,              //3 value
+							 svalueStr,             //4 svalue
+							 statusString,          //5 status
+							 null                   //6 flag
+							 );
+						lineCnt++;
+						if (lineCnt > 1e05) {
+							lineBuilder.Remove(lineBuilder.Length - 2, 2);
+							outFile.WriteLine(lineBuilder.ToString());
+							outFile.Flush();
+							lineBuilder = new StringBuilder();
+							lineCnt = 0;
+						}
+					} catch (Exception ex) {
+						logger.Error("Unable to serialize tag {0} to file. Value: {1}, date: {2}.", tag.name, valueStr, dateStr);
+						logger.Error("Details: {0}", ex.Message);
+						logger.Info("Exception: {0}", ex.ToString());
+					}
+				}
+				lineBuilder.Remove(lineBuilder.Length - 2, 2);
+				outFile.WriteLine(lineBuilder.ToString());
+				outFile.Flush();
+			} catch (Exception e) {
+				throw new Exception("Unable to serialize tag " + tag.name + " on CSV file. Details: " + e.Message);
+			}
+		}
+
+		[System.Obsolete]
 		private void serializeTagToLocalFile(Tag tag, StreamWriter outFile) {
 			//sample tag values: 2017-02-13T11-47-16:236.375,2017-02-13T11-48-12:No Data
 			//sample csv line should be: ITP_MTC_AE-066-491-01.P10.PV, 2016-08-22 11:49:03, -9.2926378, null, 0
@@ -153,7 +235,5 @@ namespace PIDataReaderLib {
 				throw new Exception("Unable to serialize tag " + tag.name + " on CSV file. Details: " + e.Message);
 			}
 		}
-
-		
 	}
 }
